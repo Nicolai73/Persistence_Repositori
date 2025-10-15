c9c5ec82fa08531d4aa0fdc389d0770ee32d6261
Nicolai73 <10647364@ucn.dk>
2025-10-15 10:14:44 +0200

    Sql_Scripts
    Create og Drop Table

---
+++ SQL_Scripts/MP Persistence Drop Table.sql
@@ -0,0 +1,34 @@
+
+drop table GunReplicaTable
+
+drop table EquipmentTable
+
+drop table ClothingTable
+
+drop table MusicTable
+
+drop table Stock
+
+drop table Warehouse
+
+drop table SalePrice
+
+drop table ProductSupplier
+
+drop table OrderlineItem
+
+drop table SaleOrder
+
+drop table Discount
+
+drop table Invoice
+
+drop table Freight
+
+drop table [Product]
+
+drop table Customer
+
+drop table Supplier
+
+drop table ZipTable
